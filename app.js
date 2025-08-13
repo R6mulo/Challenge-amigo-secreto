012dab8796e654ee13074396ad4c89b0ae1f6e6d
R6mulo <r6rorschach.dev@gmail.com>
2025-08-13 17:41:47 -0300

    Estrutura inicial

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+    // Amigo Secreto - (Estrutura inicial)
+    // Array para armazenar os nomes dos amigos
+  
+    const amigos = [];
+    
+    function adicionarAmigo() {
+        const input = document.getElementById('amigo');
+        const nome = input.ariaValueMax.trim();
+
+        // Validação de campo vazio
+        if (nome === '') {
+            alert('Por Favor, insira um nome válido');
+            return;
+        }
+
+        // Adiciona o nome ao array
+        amigos.push(nome);
+
+        // Limpa o campo de entrada
+        input.value = '';
+
+        // Atualizar a lista na tela
+        console.log('Lista de amigos:', amigos);
+    }
